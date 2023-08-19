b455c39f83b301213332eae15acac8b7977f856b
Pragya Vaishanav <40742845+pragyavaishanav@users.noreply.github.com>
2023-08-18 22:58:49 -0400

    template for running shader

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import styled from "styled-components";
+import { Canvas, extend } from "@react-three/fiber";
+import { OrbitControls, shaderMaterial } from "@react-three/drei";
+import { DirectionalLight } from "three";
+import * as THREE from "three";
+
+const StyledCanvas = styled(Canvas)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+// Define custom shader material
+const MyShaderMaterial = shaderMaterial(
+  { color: new THREE.Color(0xff0000) }, // Uniforms
+  // Vertex shader
+  `
+    varying vec3 vColor;
+    void main() {
+      vColor = color;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `,
+  // Fragment shader
+  `
+    varying vec3 vColor;
+    void main() {
+      gl_FragColor = vec4(vColor, 1.0);
+    }
+  `
+);
+
+// // Extend react-three-fiber to include the custom shader material
+extend({ MyShaderMaterial });
 
 function App() {
+  const shaderMaterial = new THREE.ShaderMaterial({
+    wireframe: true,
+    uniforms: {
+      color: { value: new THREE.Color(0xff0000) },
+    },
+    vertexShader: `
+      varying vec3 vColor;
+      uniform vec3 color;
+      void main() {
+        vColor = color;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+    fragmentShader: `
+      varying vec3 vColor;
+      void main() {
+        gl_FragColor = vec4(vColor, 1.0);
+      }
+    `,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas camera={{ position: [0, 0, 5] }}>
+      <ambientLight />
+      <mesh>
+        <boxGeometry attach="geometry" args={[2, 2, 2, 10, 10, 10]} />
+        <primitive object={shaderMaterial} />
+        {/* <meshStandardMaterial
+          attach="material"
+          color="blue"
+          wireframe={true}
+        /> */}
+      </mesh>
+      <OrbitControls />
+      <directionalLight position={[0, 10, 5]} intensity={1} />
+    </Canvas>
   );
 }
 
