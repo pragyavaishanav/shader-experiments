6c62d9732daa0c65fee58529c78948f5e27112f8
Pragya Vaishanav <40742845+pragyavaishanav@users.noreply.github.com>
2023-08-19 16:15:24 -0400

    removed the detail

--- src/elements/materials/boxToSphereMaterial.js
+++ src/elements/materials/boxToSphereMaterial.js
@@ -2,12 +2,9 @@ import * as THREE from "three";
 import vertexShader from "../shaders/vertex/boxToSphereVS.js";
 import fragmentShader from "../shaders/fragment/boxToSphereFS.js";
 
-console.log("vertexShader", vertexShader);
-console.log("fragmentShader", fragmentShader);
 const shaderMaterial = new THREE.ShaderMaterial({
   wireframe: true,
   uniforms: {
-    color: { value: new THREE.Color(0xff0000) },
     u_time: { value: 0 },
   },
   vertexShader: vertexShader,
