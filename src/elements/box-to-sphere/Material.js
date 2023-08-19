4d6deaa0953a973483b50a8119ad69c8d7cd5c6c
Pragya Vaishanav <40742845+pragyavaishanav@users.noreply.github.com>
2023-08-18 23:20:38 -0400

    box to sphere works

--- src/elements/box-to-sphere/Material.js
+++ src/elements/box-to-sphere/Material.js
@@ -4,13 +4,19 @@ const shaderMaterial = new THREE.ShaderMaterial({
   wireframe: true,
   uniforms: {
     color: { value: new THREE.Color(0xff0000) },
+    u_time: { value: 0 },
   },
   vertexShader: `
       varying vec3 vColor;
       uniform vec3 color;
+      uniform float u_time;
       void main() {
+        float radius = 1.0;
         vColor = color;
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        float delta = 0.5 + sin(u_time) * 0.5;
+        vec3 v = normalize(position) * radius;
+        vec3 pos = mix(position, v, delta);
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
       }
     `,
   fragmentShader: `
--- src/elements/box-to-sphere/Model.js
+++ src/elements/box-to-sphere/Model.js
@@ -1,6 +1,10 @@
 import shaderMaterial from "./Material";
+import { useFrame } from "@react-three/fiber";
 
 const Model = () => {
+  useFrame((state) => {
+    shaderMaterial.uniforms.u_time.value = state.clock.getElapsedTime();
+  });
   return (
     <mesh>
       <boxGeometry attach="geometry" args={[2, 2, 2, 10, 10, 10]} />
