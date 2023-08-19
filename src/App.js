a2024f860221458fbe431c04d981f5b20c0bfce5
Pragya Vaishanav <40742845+pragyavaishanav@users.noreply.github.com>
2023-08-18 23:00:56 -0400

    cleaner Code

--- src/App.js
+++ src/App.js
@@ -1,41 +1,6 @@
-// import logo from './logo.svg';
-// import './App.css';
-import styled from "styled-components";
-import { Canvas, extend } from "@react-three/fiber";
-import { OrbitControls, shaderMaterial } from "@react-three/drei";
-import { DirectionalLight } from "three";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 import * as THREE from "three";
-
-const StyledCanvas = styled(Canvas)`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-`;
-
-// Define custom shader material
-const MyShaderMaterial = shaderMaterial(
-  { color: new THREE.Color(0xff0000) }, // Uniforms
-  // Vertex shader
-  `
-    varying vec3 vColor;
-    void main() {
-      vColor = color;
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    }
-  `,
-  // Fragment shader
-  `
-    varying vec3 vColor;
-    void main() {
-      gl_FragColor = vec4(vColor, 1.0);
-    }
-  `
-);
-
-// // Extend react-three-fiber to include the custom shader material
-extend({ MyShaderMaterial });
 
 function App() {
   const shaderMaterial = new THREE.ShaderMaterial({
@@ -65,11 +30,6 @@ function App() {
       <mesh>
         <boxGeometry attach="geometry" args={[2, 2, 2, 10, 10, 10]} />
         <primitive object={shaderMaterial} />
-        {/* <meshStandardMaterial
-          attach="material"
-          color="blue"
-          wireframe={true}
-        /> */}
       </mesh>
       <OrbitControls />
       <directionalLight position={[0, 10, 5]} intensity={1} />
